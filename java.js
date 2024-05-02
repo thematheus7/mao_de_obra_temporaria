e102df4b095baa977ca3bb2eecabcfddaad41eb6
thematheus7 <144571302+thematheus7@users.noreply.github.com>
2024-05-02 17:05:31 -0400

    Add files via upload

---
+++ java.js
@@ -0,0 +1,19 @@
+document.getElementById('home-btn').addEventListener('click', function() {
+    alert('Você está na página inicial.');
+});
+
+document.getElementById('offer-btn').addEventListener('click', function() {
+    alert('Ofertas serão exibidas em breve!');
+});
+
+document.getElementById('about-btn').addEventListener('click', function() {
+    alert('Saiba mais sobre nossa empresa.');
+});
+
+document.getElementById('contact-btn').addEventListener('click', function() {
+    alert('Entre em contato conosco.');
+});
+
+document.getElementById('job-btn').addEventListener('click', function(){
+  alert('Logo teremos mão de obra disponível!');
+});
